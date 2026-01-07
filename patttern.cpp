7bffe9031d125f239477ce161b9103dc51374ffc
Sarvesh Naik <naiksarvesh463@gmail.com>
2026-01-07 12:07:41 +0530

    Pattern Questions

---
+++ patttern.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+void printTriangleAsc(int rows) {
+    for(int i = 1; i <= rows; i++) {
+        // Print Space
+        for(int j = i; j <= rows; j++) {
+            cout << " ";
+        }
+
+        // Print Star
+        for(int k = 1; k <= i; k++) {
+            cout << "* "; 
+        }
+
+        cout << endl;
+    }
+}
+
+void printTriangleDesc(int rows) {
+    for(int i = 1; i <= rows; i++) {
+        // Print Space
+        for(int k = 1; k <= i; k++) {
+            cout << " "; 
+        }
+
+        // Print Star
+        for(int j = i; j <= rows; j++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+}
+
+void printTriangleBoth(int rows) {
+    // 1st half
+    for(int i = 1; i <= rows; i++) {
+        // Print Space
+        for(int j = i; j <= rows; j++) {
+            cout << " ";
+        }
+
+        // Print Star
+        for(int k = 1; k <= i; k++) {
+            cout << "* "; 
+        }
+
+        cout << endl;
+    }
+
+    // 2nd Half
+    for(int i = 1; i <= rows; i++) {
+        // Print Space
+        for(int k = 1; k <= i; k++) {
+            cout << " "; 
+        }
+
+        // Print Star
+        for(int j = i; j <= rows; j++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+}
+
+int main() {
+    // printTriangleAsc(5);
+    printTriangleBoth(5);
+}
