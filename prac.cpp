9ffa62412f732609eb2b3edd97512fd9c570819e
Sarvesh Naik <naiksarvesh463@gmail.com>
2026-01-12 16:59:28 +0530

    Pattern

---
+++ prac.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Woring of pair (also works with nested pair)
+    pair<int, int> arr[] = {{1, 2}, {3, 4}, {5, 6}};
+    cout << arr[0].first << " " << arr[2].second;
+
+    return 0;
+}
