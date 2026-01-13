3ae349104f5d333cd1e5ab51f907e8604ac70f7a
Sarvesh Naik <naiksarvesh463@gmail.com>
2026-01-13 11:58:12 +0530

    Factorial

--- recursion.cpp
+++ recursion.cpp
@@ -1,8 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+int factorial(int num) {
+    if(num == 1) return 1;
+    return num * factorial(num - 1);
+}
 
+int main() {
+    int fact = factorial(5);
+    cout << fact;
+    cout << endl;
     return 0;
 }
