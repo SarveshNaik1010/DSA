60306700e258a66040411054ff98b3e30d6be39c
Sarvesh Naik <naiksarvesh463@gmail.com>
2026-01-15 16:56:24 +0530

    Recursion

--- recursion.cpp
+++ recursion.cpp
@@ -13,11 +13,77 @@ void name(string inputName, int num) {
     return;
 }
 
+void oneToN(int i, int n) {
+    if(i > n) return;
+    cout << i << " ";
+    oneToN(i+1 , n);
+}
+
+void nTo1(int n) {
+    if(n < 1) return;
+    cout << n << " "; 
+    nTo1(n - 1);
+}
+
+int sumOf1stN(int i, int n) {
+    if(i == n) return n;
+    return i + sumOf1stN(i + 1, n);
+}
+
+void reverse(int i, int arr[], int n) {
+    if(i >= n / 2) return;
+    swap(arr[i], arr[n - i - 1]);
+    reverse(i + 1, arr, n);
+}
+
+// int isPalindrome(int i, string s, int n, int correctCount = 0) {
+//     if(i >= n / 2) {
+//         if(correctCount >= n / 2) cout << "Palindome";
+//         else cout << "Not Palindrome";
+//         return 0;
+//     };
+//     if(s[i] == s[n - i - 1]) correctCount++;
+//     isPalindrome(i + 1, s, n, correctCount);
+//     return correctCount;
+// }
+
+bool isPalindrome(int i, string s, int n) {
+    if(i >= n / 2) return true;
+    if(s[i] != s[n - i - 1]) return false;
+    return isPalindrome(i + 1, s, n);   
+}
+
+int fibo(int low, int up, int range) {
+
+}
+
 int main() {
     int fact = factorial(5);
     cout << fact;
     cout << endl;
-
     name("Sarvesh", 12);
+    cout << endl;
+
+    oneToN(1, 5);
+    cout << endl;
+    
+    nTo1(45);
+    cout << endl;
+    
+    cout << sumOf1stN(1, 7) << " ";
+    cout << endl;
+
+    int n = 5;
+    int arr[5] = {1, 2, 3, 4, 5};
+    reverse(0, arr, n);
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout <<  isPalindrome(0, "tenet", 5);
+    cout << endl;
+
+
     return 0;
 }
