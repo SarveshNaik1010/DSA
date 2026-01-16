91aae8be278a6485481de149101ef5799b41a385
Sarvesh Naik <naiksarvesh463@gmail.com>
2026-01-16 23:28:20 +0530

    Partial stage

---
+++ hashing.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[] = {2, 7, 3, 8, 12, 5, 10, 2, 7, 3, 3};
+    int n = sizeof(arr);
+
+    // Precompute
+    int hash[12] = {0};
+    for(int i = 0; i < n; i++) {
+        hash[arr[i]] += 1;
+    }
+
+    // Take the number from the user
+    int q = 3;
+    while(q--) {
+        int num;
+        cin >> num;
+        // Fetch
+        cout << hash[q];
+    }
+
+    return 0;
+}
