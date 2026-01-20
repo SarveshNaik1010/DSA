656e437d2677e8e62e3684d1591fb84cee2edb61
Sarvesh Naik <naiksarvesh463@gmail.com>
2026-01-20 13:40:00 +0530

    Set

--- revision.cpp
+++ revision.cpp
@@ -2,7 +2,23 @@
 using namespace std;
 
 int main() {
-    // pattern
+    // STL
+    // 1. Given N elements print them in sorted order
+    int n;
+    set<int> s;
+    cout << "Enter the max N ";
+    cin >> n;
+
+    cout << "Enter the elements\n";
+    for(int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        s.insert(num);
+    }
+
+    for(auto it = s.begin(); it != s.end(); it++) {
+        cout << *it << " ";
+    }
 
     return 0;
 }
