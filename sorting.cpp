2b0455c341c4f3945e4f4d7a9563c57f03cd7ec1
Sarvesh Naik <naiksarvesh463@gmail.com>
2026-01-30 14:35:57 +0530

    Bubble sort

--- sorting.cpp
+++ sorting.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void printArray(int arr[], int n) {
+    // Sorted
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 void selection(int arr[], int n) {
     // Sorting
     for(int i = 0; i < n; i++) {
@@ -17,16 +25,33 @@ void selection(int arr[], int n) {
     }
 
     // Sorted
-    for(int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+   printArray(arr, n);
+}
+
+void bubble(int arr[], int n) {
+    int iter = 0;
+    for(int i = 0; i < n - 1; i++) {
+        bool isSwapped = false;
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                isSwapped = true;
+            }
+            iter++;
+        }
+        if(!isSwapped) break;
     }
+    printArray(arr, n);
+    cout << "Iterations: " << iter;
 }
 
 int main() {
-    int arr[] = {5, 1, 4, 2, 8, 0, 2};
+    // int arr[] = {13, 7, 4, 17, 9, 2, 18, 5};
+    int arr[] = {1, 3, 2, 4, 5};
     int size = sizeof(arr) / sizeof(arr[0]);
     
-    selection(arr, size);
+    // selection(arr, size);
+    bubble(arr, size);
 
 
     return 0;
